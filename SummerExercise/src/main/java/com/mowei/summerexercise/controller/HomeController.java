33b51a9d0d739ff71a03ae8077570e5d15878694
Mowei <152236585+justamowei@users.noreply.github.com>
2024-08-20 15:39:58 +0800

    Update HomeController.java

--- SummerExercise/src/main/java/com/mowei/summerexercise/controller/HomeController.java
+++ SummerExercise/src/main/java/com/mowei/summerexercise/controller/HomeController.java
@@ -8,6 +8,6 @@ public class HomeController {
 
     @GetMapping("/")
     public String home() {
-        return "RWD/RWDExercise.html"; // 根據靜態文件的實際路徑來設置
+        return "RWD/RWDExercise.html"; 
     }
 }
